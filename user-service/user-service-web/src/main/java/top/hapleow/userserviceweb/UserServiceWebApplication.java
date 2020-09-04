5ee3fb3f69321cb8a32a9ed3d9489de2a3f630b7
HapLeo <hapleow@gmail.com>
2020-09-04 17:20:45 +0800

    user-service-web

---
+++ user-service/user-service-web/src/main/java/top/hapleow/userserviceweb/UserServiceWebApplication.java
@@ -0,0 +1,13 @@
+package top.hapleow.userserviceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceWebApplication.class, args);
+    }
+
+}
---
+++ user-service/user-service-web/src/main/java/top/hapleow/userserviceweb/controller/UserServiceController.java
@@ -0,0 +1,21 @@
+package top.hapleow.userserviceweb.controller;
+
+import org.apache.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.hapleow.service.IUserService;
+
+@RestController
+@RequestMapping("/user")
+public class UserServiceController {
+
+    @Reference
+    private IUserService userService;
+
+    @RequestMapping("/list")
+    public Object list() {
+        return userService.list();
+    }
+
+
+}
