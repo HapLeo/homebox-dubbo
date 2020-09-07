782d518f050255341e68feb10ff07d9e07a88c90
HapLeo <hapleow@gmail.com>
2020-09-07 14:59:58 +0800

    config mybatis-plus.

--- user-service/user-service-app/src/main/java/top/hapleow/userserviceapp/UserServiceAppApplication.java
+++ user-service/user-service-app/src/main/java/top/hapleow/userserviceapp/UserServiceAppApplication.java
@@ -1,9 +1,11 @@
 package top.hapleow.userserviceapp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "top.hapleow.userserviceapp.dao")
 public class UserServiceAppApplication {
 
     public static void main(String[] args) {
