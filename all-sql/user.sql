ce87008ba23f29ba114adeea08ae039b540de626
HapLeo <hapleow@gmail.com>
2020-09-07 14:58:08 +0800

    modify sql column.

--- all-sql/user.sql
+++ all-sql/user.sql
@@ -3,9 +3,9 @@ create table user
     user_id            bigint auto_increment comment '用户ID'
         primary key,
     user_name          varchar(200)              default ''                null comment '用户名称',
-    user_seq_id        varchar(30)               default ''                null comment '用户唯一识别号',
+    user_seq_id        varchar(64)               default ''                null comment '用户唯一识别号',
     user_nick_name     varchar(30)               default ''                null comment '用户昵称',
-    user_password      varchar(30)               default ''                null comment '用户密码',
+    user_password      varchar(64)               default ''                null comment '用户密码',
     user_password_salt varchar(6)                default ''                null comment '用户密码加盐',
     user_telephone     varchar(18)               default ''                null comment '用户手机号',
     user_idcard_no     varchar(18)               default ''                null comment '用户身份证号',
--- user-service/user-service-web/src/main/java/top/hapleow/userserviceweb/controller/UserServiceController.java
+++ user-service/user-service-web/src/main/java/top/hapleow/userserviceweb/controller/UserServiceController.java
@@ -3,6 +3,7 @@ package top.hapleow.userserviceweb.controller;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.hapleow.model.User;
 import top.hapleow.service.IUserService;
 
 @RestController
@@ -17,5 +18,16 @@ public class UserServiceController {
         return userService.list();
     }
 
+    @RequestMapping("/add")
+    public Object add(User user) {
+        userService.addItem(user);
+        return "SUCCESS";
+    }
+
+    @RequestMapping("/edit")
+    public Object edit(User user) {
+        userService.editItem(user);
+        return "SUCCESS";
+    }
 
 }
